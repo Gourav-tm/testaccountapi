826d4751759c7a06f1d02a1c327116c8e9913a69
Gourav Jhangikhel <Gourav.Jhangikhel@teramatrix.local>
2022-08-01 06:15:38 +0530

    Remove Required condition from parent vendor

--- src/vendor/dto/vendor-create.dto.ts
+++ src/vendor/dto/vendor-create.dto.ts
@@ -1,7 +1,5 @@
-import { IsString } from 'class-validator';
 import { CommonVendorProject } from 'src/shared/commonvendorproject';
 
 export class CreateVendorDto extends CommonVendorProject {
-  @IsString()
   parentVendorId: string;
 }
